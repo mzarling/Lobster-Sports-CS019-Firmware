61d36f0c2a7c45cf2b45c5772f33dcaeca2712b6
mzarling <Michael.Zarling@MichaelZarlingDesign.com>
2025-09-17 14:08:55 -0700

    Migrated CS019B05 project from MPLAB IDE v8.92 to MPLAB X IDE v5.35
    New migrated project directory is CS019-TwoWheel.
    Generated hex file is identical to CS019B05.HEX.
    MPLAB X IDE v5.35 is last version to ship with MPASM assembler.
    Subsequent versions shipped with PIC-AS assembler, which is not syntax compatible with MPASM.

---
+++ CS019-TwoWheel/CS019-TwoWheel.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=CS019-TwoWheel.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/CS019-TwoWheel.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=cs019-twowheel.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/cs019-twowheel.x.tar
